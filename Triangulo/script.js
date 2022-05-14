eab8658e530a8b74d22302db0e9f987366ed75ff
Yanick Santos <yanickeduardo04@outlook.com>
2022-05-14 10:36:33 -0300

    leyout e logica finalizada

---
+++ Triangulo/script.js
@@ -0,0 +1,14 @@
+const $ladoA = document.querySelector("#ladoA")
+const $ladoB = document.querySelector("#ladoB")
+const $ladoC = document.querySelector("#ladoC")
+
+function DefinirTriangulo(){
+	if(ladoA == ladoB && ladoB == ladoC){
+	} 
+    else {
+		if(ladoA != ladoB && ladoB != ladoC && ladoA != ladoC) {
+		} else {
+            
+        }
+	}
+}
