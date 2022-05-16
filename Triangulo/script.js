da2d71d92fcc03a3c8ec4788d6944dba7637546f
Yanick Santos <yanickeduardo04@outlook.com>
2022-05-15 23:20:10 -0300

    finalizado triangulo

--- Triangulo/script.js
+++ Triangulo/script.js
@@ -1,14 +1,53 @@
 const $ladoA = document.querySelector("#ladoA")
 const $ladoB = document.querySelector("#ladoB")
 const $ladoC = document.querySelector("#ladoC")
+const $resultado = document.querySelector("#resultado")
+const $imagem = document.querySelector("img")
+const $span = document.querySelector("span")
 
-function DefinirTriangulo(){
-	if(ladoA == ladoB && ladoB == ladoC){
-	} 
-    else {
-		if(ladoA != ladoB && ladoB != ladoC && ladoA != ladoC) {
-		} else {
-            
-        }
+function definirTriangulo(){
+	console.log()
+	if($ladoA.value.length == 0 || $ladoB.value.length == 0 || $ladoC.value.length == 0){
+		alert("Existe campo vazio")
+	}
+	else{
+		$resultado.style.display="block"
+		if(ladoA.value == ladoB.value && ladoB.value == ladoC.value){
+			$imagem.src="img/triangulo-equilatero.png"
+			$span.textContent="EQUILÁTERO"
+		} 
+		else {
+			if(ladoA.value != ladoB.value && ladoB.value != ladoC.value && ladoA.value != ladoC.value) {
+				$imagem.src="img/triangulo-escaleno.png"
+				$span.textContent="ESCALENO"
+			} else {
+				$span.textContent="ISOSCELE"
+				
+				if($ladoA.value == $ladoB.value){
+					if($ladoA.value > $ladoC.value){
+						$imagem.src="img/triangulo-isoscele-base-menor.png"
+					}
+					else{
+						$imagem.src="img/Triángulo-Isosceles-base-maior.jpg"
+					}
+				}
+				else if($ladoA.value == $ladoC.value){
+					if($ladoA.value > $ladoB.value){
+						$imagem.src="img/triangulo-isoscele-base-menor.png"
+					}
+					else{
+						$imagem.src="img/Triángulo-Isosceles-base-maior.jpg"
+					}
+				}
+				else{
+					if($ladoB.value > $ladoA.value){
+						$imagem.src="img/triangulo-isoscele-base-menor.png"
+					}
+					else{
+						$imagem.src="img/Triángulo-Isosceles-base-maior.jpg"
+					}
+				}	
+			}
+		}
 	}
 }
